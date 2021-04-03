aff1f108d51427d8f03e03d285b6932aa8c275f0
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-03 19:13:16 +0200

    Update and rename Temp - Copy (18).py to FindingThePercentage.py

---
+++ PythonSolutions/FindingThePercentage.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    sum = 0
+    for item in student_marks[query_name]:
+        sum += float(item)  
+    print('%.2f'%(sum/3))
