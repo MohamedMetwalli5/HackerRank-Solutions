a55b3803139a38dae326ba0123d255d8db471f21
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:43:47 +0200

    Update JavaStringReverse.java

--- ProblemSolving/src/solution/JavaStringReverse.java
+++ ProblemSolving/src/solution/JavaStringReverse.java
@@ -1,1 +1,24 @@
-f
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int i=0, j=A.length()-1;
+        while(i<=j){
+           if(A.charAt(i) != A.charAt(j)){
+               System.out.println("No");
+               System.exit(0);      
+           }
+           i++;
+           j--;
+        }
+        System.out.println("Yes"); 
+    }
+}
+
+
+
