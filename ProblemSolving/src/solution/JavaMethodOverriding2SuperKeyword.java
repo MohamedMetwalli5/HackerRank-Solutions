4268698a35ab3ede1918279d3b6473ed048afda2
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:40:59 +0200

    Update and rename JavaMethodOverriding2(SuperKeyword).java to JavaMethodOverriding2SuperKeyword.java

---
+++ ProblemSolving/src/solution/JavaMethodOverriding2SuperKeyword.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+    String temp=super.define_me();
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class JavaMethodOverriding2SuperKeyword{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
