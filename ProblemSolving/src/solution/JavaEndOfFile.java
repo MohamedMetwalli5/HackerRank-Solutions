a1e8ecd567bce93870495fbe755416b5cf799b89
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:21:22 +0200

    Update JavaEndOfFile.java

--- ProblemSolving/src/solution/JavaEndOfFile.java
+++ ProblemSolving/src/solution/JavaEndOfFile.java
@@ -1,1 +1,20 @@
-f
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaEndOfFile {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counter = 0;
+        while(input.hasNextLine()){
+          String s = input.nextLine();
+          counter++;
+          System.out.print(counter + " ");
+          System.out.println(s);
+
+        }
+    }
+}
