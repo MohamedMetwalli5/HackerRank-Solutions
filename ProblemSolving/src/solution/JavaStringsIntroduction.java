bf6ef272eb188cae03781cc23f96793bf35a534e
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:45:39 +0200

    Update JavaStringsIntroduction.java

--- ProblemSolving/src/solution/JavaStringsIntroduction.java
+++ ProblemSolving/src/solution/JavaStringsIntroduction.java
@@ -1,1 +1,24 @@
-f
+import java.io.*;
+import java.util.*;
+
+public class JavaStringsIntroduction {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length() + B.length());
+        if(A.compareTo(B) > 0){
+           System.out.println("Yes");
+        }else{
+           System.out.println("No");
+        }
+        
+        System.out.println(Character.toUpperCase(A.charAt(0)) + A.substring(1, A.length()) + " " + Character.toUpperCase(B.charAt(0)) + B.substring(1, B.length()));
+           
+    }
+}
+
+
+
