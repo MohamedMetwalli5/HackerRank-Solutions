2eefa2a86fda27e98affe40774abeb7fb702f0c1
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:19:03 +0200

    Update JavaDatatypes.java

--- ProblemSolving/src/solution/JavaDatatypes.java
+++ ProblemSolving/src/solution/JavaDatatypes.java
@@ -1,1 +1,46 @@
-f
+import java.util.*;
+import java.io.*;
+
+
+
+class JavaDatatypes{
+    public static void main(String []argh){
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+              
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                if(x>= -(Math.pow(2,15)) && x<= (Math.pow(2,15)-1)){
+                   System.out.println("* short");
+                   System.out.println("* int");
+                   System.out.println("* long"); 
+                
+                }else if(x >= -(Math.pow(2,31)) && x<= (Math.pow(2,31)-1)){ 
+                   System.out.println("* int");
+                   System.out.println("* long");
+                
+                }else if(x >= -Math.pow(2,63) && x<= (Math.pow(2,63)-1)){
+                   System.out.println("* long");
+                }
+              }
+                
+                //Complete the code
+            
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
+
+
