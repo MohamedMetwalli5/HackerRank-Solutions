cc152d0fde08ab26e919dca1440a50b32955aa3b
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:51:26 +0200

    Update PatternSyntaxChecker.java

--- ProblemSolving/src/solution/PatternSyntaxChecker.java
+++ ProblemSolving/src/solution/PatternSyntaxChecker.java
@@ -1,1 +1,24 @@
-f
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class PatternSyntaxChecker
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases > 0){
+			String s = in.nextLine();
+          	try{
+                Pattern p = Pattern.compile(s);  
+                System.out.println("Valid");  
+            }catch(Exception e){
+                System.out.println("Invalid");
+            }
+            //Write your code
+            testCases--;
+		}
+	}
+}
+
+
+
