13d797b2b36c9d4cf25310d37309df6d4aa02bfc
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:22:57 +0200

    Update JavaIfElse.java

--- ProblemSolving/src/solution/JavaIfElse.java
+++ ProblemSolving/src/solution/JavaIfElse.java
@@ -1,1 +1,32 @@
-f
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class JavaIfElse {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        if(N%2!=0){
+            System.out.println("Weird");
+        }else if(N%2==0){
+             if(N>=2&&N<=5){
+               System.out.println("Not Weird");
+             }else if(N>=6&&N<=20){
+               System.out.println("Weird");   
+             }else if(N>20){
+               System.out.println("Not Weird");  
+             }
+        }
+    }
+}
