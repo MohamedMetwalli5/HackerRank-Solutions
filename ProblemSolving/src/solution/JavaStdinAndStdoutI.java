ddb444f05a68110e48f74e1d59fa5568620fc2e5
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:42:29 +0200

    Update JavaStdinAndStdoutI.java

--- ProblemSolving/src/solution/JavaStdinAndStdoutI.java
+++ ProblemSolving/src/solution/JavaStdinAndStdoutI.java
@@ -1,1 +1,16 @@
-f
+import java.util.*;
+
+public class JavaStdinAndStdoutI {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();// Complete this line
+        int c = scan.nextInt();// Complete this line
+
+        System.out.println(a);
+        System.out.println(b);// Complete this line
+        System.out.println(c);// Complete this line
+    }
+}
+
