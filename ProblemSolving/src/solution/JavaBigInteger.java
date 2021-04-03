49e1c8bba41ccc433516f8794c0e52baecb10490
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:15:44 +0200

    Update JavaBigInteger.java

--- ProblemSolving/src/solution/JavaBigInteger.java
+++ ProblemSolving/src/solution/JavaBigInteger.java
@@ -1,1 +1,18 @@
-f
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaBigInteger {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        BigInteger a = input.nextBigInteger();
+        BigInteger b = input.nextBigInteger();
+        
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
