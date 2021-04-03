39fa83e39c64bbbc3f3c9f1fb926946e7c5d01d9
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:09:17 +0200

    Update Java1DArray.java

--- ProblemSolving/src/solution/Java1DArray.java
+++ ProblemSolving/src/solution/Java1DArray.java
@@ -1,1 +1,21 @@
-f
+import java.util.*;
+
+public class Java1DArray {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = scan.nextInt();
+        }
+
+        scan.close();
+      
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
