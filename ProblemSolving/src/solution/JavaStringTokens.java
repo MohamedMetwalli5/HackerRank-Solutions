f4262d6b541d73c05f8066ae36708ddbc291a47f
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:44:46 +0200

    Update JavaStringTokens.java

--- ProblemSolving/src/solution/JavaStringTokens.java
+++ ProblemSolving/src/solution/JavaStringTokens.java
@@ -1,1 +1,22 @@
-f
+import java.io.*;
+import java.util.*;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        if(s.trim().length() != 0){
+            s = s.trim();
+            String[] array = s.split("[ .,'?!@_]+");
+            System.out.println(array.length);
+            for(int i=0;i<array.length;i++){
+                System.out.println(array[i]);
+            }
+        }else{
+            System.out.print("0");
+        }
+        
+    }
+}
+
