edc5470a9de6c9bf42744e73c579045da0f033e0
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:47:07 +0200

    Update JavaSubstring.java

--- ProblemSolving/src/solution/JavaSubstring.java
+++ ProblemSolving/src/solution/JavaSubstring.java
@@ -1,1 +1,16 @@
-d
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        System.out.println(S.substring(start,end));
+    }
+}
