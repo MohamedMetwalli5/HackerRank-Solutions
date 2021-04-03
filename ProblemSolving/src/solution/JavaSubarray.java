319713827b95f3e17073bf0572e094636d190d39
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:46:20 +0200

    Update JavaSubarray.java

--- ProblemSolving/src/solution/JavaSubarray.java
+++ ProblemSolving/src/solution/JavaSubarray.java
@@ -1,1 +1,29 @@
-f
+import java.io.*;
+import java.util.*;
+
+public class JavaSubarray {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = input.nextInt();
+        }
+        int counter = 0;
+        // Arrays.sort(arr);
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j=i;j<n;j++){
+                sum += arr[j];
+                if(sum < 0){
+                    counter++;
+                }
+            }
+            sum = 0;
+        }
+        
+        System.out.println(counter);
+    }
+}
