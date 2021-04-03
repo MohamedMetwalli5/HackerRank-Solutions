1c2f1e3c2160d97becc4d77c392f984361eb8409
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-03 21:39:38 +0200

    Delete GroupGroupsGroupdict.py

--- PythonSolutions/GroupGroupsGroupdict.py
+++
@@ -1,9 +0,0 @@
-s = input()
-flag =0 
-for i in range(len(s)-1):
-    if s[i].isalnum() and s[i+1].isalnum() and s[i] == s[i+1]:
-        print(s[i])
-        flag = 1
-        break
-if flag == 0:
-    print(-1)
