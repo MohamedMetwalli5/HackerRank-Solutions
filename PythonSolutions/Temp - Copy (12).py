b6c8e31266ad8ee04b2e56b7480b5412e5bf4426
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-03 19:07:17 +0200

    Update Temp - Copy (12).py

--- PythonSolutions/Temp - Copy (12).py
+++ PythonSolutions/Temp - Copy (12).py
@@ -1,1 +1,10 @@
-print("tempo")
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+    if n%2!=0 : 
+        print("Weird")
+    if n%2==0 and n>=2 and n<=5 :
+      print("Not Weird")
+    if n%2==0 and n>=6 and n<=20 :
+        print("Weird")
+    if n%2==0 and n>20 :
+        print("Not Weird")
