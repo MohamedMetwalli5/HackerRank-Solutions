694f4056ab66c27c936ef5ffd343cb0ed13e01f4
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-03 19:02:28 +0200

    Update and rename Temp - Copy (10).py to NoIdea.py

---
+++ PythonSolutions/NoIdea.py
@@ -0,0 +1,6 @@
+n,m = (map(int,input().split()))
+l = list(map(int,input().split()))
+a = set(map(int,input().split()))
+b = set(map(int,input().split()))
+counter = sum([(i in a) - (i in b) for i in l])
+print(counter)
