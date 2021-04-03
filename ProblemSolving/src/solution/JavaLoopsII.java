715cf5b9d85b89c25d05c15d1768bbf6fa1f7421
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:38:47 +0200

    Update JavaLoopsII.java

--- ProblemSolving/src/solution/JavaLoopsII.java
+++ ProblemSolving/src/solution/JavaLoopsII.java
@@ -1,1 +1,23 @@
-f
+import java.util.*;
+import java.io.*;
+
+class JavaLoopsII{
+    public static void main(String []argh){
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        for(int i=0;i<t;i++){
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int n = input.nextInt();
+            int sum = 0, counter = 0;
+            while(counter <= n-1) {
+                sum += (int)Math.pow(2, counter)*b;
+                System.out.print(a + sum);
+                System.out.print(" ");
+                counter++;
+            }
+            System.out.println();
+        
+        }
+    }
+}
