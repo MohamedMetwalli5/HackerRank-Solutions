0894154c98d3d3725f8b7066fc4de60829b8ea88
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 01:38:11 +0200

    Update JavaLoopsI.java

--- ProblemSolving/src/solution/JavaLoopsI.java
+++ ProblemSolving/src/solution/JavaLoopsI.java
@@ -1,1 +1,26 @@
-d
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class JavaLoopsI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int i;
+        int z;
+        for(i=1;i<=10;i++){
+            z = (N)*(i);
+           System.out.printf("%d x %d = %d\n",N,i,z);
+        }
+        scanner.close();
+    
+    }
+
+}
