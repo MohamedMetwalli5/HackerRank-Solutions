279ceee0fc016ed73a3defb8d7ffe6042b41cd0d
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 04:41:13 +0200

    Update and rename Temp - Copy (26).py to SequenceEquation.java

---
+++ ProblemSolving (General)/SequenceEquation.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SequenceEquation {
+
+    // Complete the permutationEquation function below.
+    static int[] permutationEquation(int[] p) {
+       int[] result = new int[p.length];
+       HashMap<Integer,Integer> hInside = new HashMap<Integer,Integer>();
+       HashMap<Integer,Integer> hOutside = new HashMap<Integer,Integer>();
+       int counter = 1;
+       for(int i=0;i<p.length;i++){
+          hOutside.put(p[i],i+1);
+       }
+       for(int i=0;i<result.length;i++){
+          int temp = hOutside.get(counter);
+          result[i] = hOutside.get(temp);
+          counter++;
+       }
+       return result;
+       
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] p = new int[n];
+
+        String[] pItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pItem = Integer.parseInt(pItems[i]);
+            p[i] = pItem;
+        }
+
+        int[] result = permutationEquation(p);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
