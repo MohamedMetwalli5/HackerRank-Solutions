914c58094d13ccabf30b48a8041880b6f1b88ea0
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 04:06:21 +0200

    Update and rename Temp - Copy (10).py to TwoStrings.java

---
+++ ProblemSolving (General)/TwoStrings.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class TwoStrings {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        
+        HashSet<Character> h = new HashSet<>();
+        for(int i=0;i<s1.length();i++){
+            h.add(s1.charAt(i));
+        }
+
+        for(int i=0;i<s2.length();i++){
+            if(h.contains(s2.charAt(i))){
+               return "YES";
+            }
+        }
+
+        return "NO"; 
+        
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
