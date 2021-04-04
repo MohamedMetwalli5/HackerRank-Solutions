3580328e4e49cededc369ba103e26203f102f332
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 04:24:45 +0200

    Update and rename Temp - Copy (14).py to BreakingTheRecords.java

---
+++ ProblemSolving (General)/BreakingTheRecords.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BreakingTheRecords {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+       int min = scores[0], max = scores[0] , maxcounter = 0, mincounter = 0;  
+       for(int i=1;i<scores.length;i++){
+          if(scores[i] > max){
+             max = scores[i];
+             maxcounter++;
+          }
+          if(scores[i] < min){
+             min = scores[i];
+             mincounter++;
+          }
+       }
+
+       int[] array = {maxcounter,mincounter};  
+       return array;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
