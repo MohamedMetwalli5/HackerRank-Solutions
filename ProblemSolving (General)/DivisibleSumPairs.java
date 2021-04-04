7b88a56a10329ba70b481e45557e8ef0b442e0e5
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 06:31:13 +0200

    Update and rename Temp - Copy (8).py to DivisibleSumPairs.java

---
+++ ProblemSolving (General)/DivisibleSumPairs.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class DivisibleSumPairs {
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+       int counter = 0;
+       for(int i=0;i<ar.length;i++){
+          for(int j=i+1;j<ar.length;j++){
+             if((ar[i] + ar[j]) %k == 0){
+                counter++;
+             }
+          }  
+       }     
+
+       return counter;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
