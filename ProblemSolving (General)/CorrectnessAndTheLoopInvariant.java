151ea63d059179e8433eca80e68a2c49e08a4dc7
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 03:42:42 +0200

    Update and rename Encryption - Copy (3).java to CorrectnessAndTheLoopInvariant.java

---
+++ ProblemSolving (General)/CorrectnessAndTheLoopInvariant.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class CorrectnessAndTheLoopInvariant {
+
+    public static void insertionSort(int[] A){
+        Arrays.sort(A);
+        printArray(A);
+    }
+
+
+    static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++){
+            ar[i]=in.nextInt();
+        }
+        insertionSort(ar);
+    }
+}
