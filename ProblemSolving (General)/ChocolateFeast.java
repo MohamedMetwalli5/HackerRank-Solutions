2ef0d8b20d859193275a369715182144c2a7283f
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 04:47:57 +0200

    Update and rename Temp - Copy (8).py to ChocolateFeast.java

---
+++ ProblemSolving (General)/ChocolateFeast.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class ChocolateFeast  {
+
+    // Complete the chocolateFeast function below.
+    static int chocolateFeast(int money, int cost, int m) {
+       int wrappers = money/cost , total = money/cost;
+       while(wrappers >= m){
+           total += wrappers/m;
+           wrappers = wrappers / m + wrappers % m;
+       }
+       return total;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] ncm = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(ncm[0]);
+
+            int c = Integer.parseInt(ncm[1]);
+
+            int m = Integer.parseInt(ncm[2]);
+
+            int result = chocolateFeast(n, c, m);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
