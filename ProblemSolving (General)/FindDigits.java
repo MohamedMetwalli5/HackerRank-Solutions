130959efee2e63d21cc0e66a2908fc290beee1b2
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 04:34:42 +0200

    Update and rename Temp - Copy (5).py to FindDigits.java

---
+++ ProblemSolving (General)/FindDigits.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class FindDigits {
+
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+      int temp = n, digit = 0 , result = 0;
+      while(temp > 0){
+         digit = temp%10;
+         temp /= 10;
+         if(digit != 0 && n % digit == 0){
+            result++;
+         }
+      }
+
+      return result; 
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = findDigits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
