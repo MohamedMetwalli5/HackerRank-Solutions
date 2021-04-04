5d1ef8e4dcf2b9823cd861932609f7d08191adce
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-04-04 04:56:08 +0200

    Update and rename Temp - Copy (5).py to CircularArrayRotation.java

---
+++ ProblemSolving (General)/CircularArrayRotation.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class CircularArrayRotation {
+
+    // Complete the circularArrayRotation function below.
+    static int[] circularArrayRotation(int[] a, int k, int[] queries) {
+          int[] array = new int[a.length];
+          for(int i=0;i<a.length;i++){
+            array[(i+k)%a.length] = a[i];
+          }
+          
+          for(int i=0;i<queries.length;i++){
+             queries[i] = array[queries[i]];
+          }
+
+          return queries;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nkq = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nkq[0]);
+
+        int k = Integer.parseInt(nkq[1]);
+
+        int q = Integer.parseInt(nkq[2]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++) {
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
